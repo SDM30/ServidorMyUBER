123ada98c48100454d50ab54ddc5fef272fb54b0
SDM30 <simondiaz@javeriana.edu.co>
2025-03-21 16:30:40 -0500

    Completado el MainServidor

--- src/main/java/org/sistemasdistribuidos/MainServidor.java
+++ src/main/java/org/sistemasdistribuidos/MainServidor.java
@@ -1,10 +1,25 @@
 package org.sistemasdistribuidos;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /*
 * Autores: Melissa Ruiz, Juan Luis y Simon Diaz
  */
 public class MainServidor {
     public static void main(String[] args) {
+        try {
+            //Crear RMI registry en el puerto 1099
+            Registry registry = LocateRegistry.createRegistry (1099);
+            System.out.println("RMI Registry creado en el puerto 1099");
 
+            //el servidor MyUBER se registra en el regitry
+            registry.rebind ("ObjetoRemotoMyUBER", new MyUBERImpl());
+            System.out.println("Objeto Remoto Creado");
+        } catch (RemoteException re) {
+            System.err.println ("Error: " + re);
+        }
     }
 }
